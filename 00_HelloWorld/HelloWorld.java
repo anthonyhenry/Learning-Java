ae9512825682987f1219d659e67e65b04915095e
anthonyhenry <anthonyh1497@gmail.com>
2024-09-04 22:25:37 -0700

    Create HelloWorld.java

---
+++ 00_HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+// Setting up a Java development environment
+    // To start developing with Java you must first install a Java Development Kit (JDK). You can find the latest by searching JDK online
+    // With a JDK you can run .java programs in terminal by navigating to their directory then running javac HelloWorld.java to compile the program, then java HelloWorld.java to execute the program
+
+// Settung up Java development with VS Code
+    // Follow the steps at this link to enable Java development with VS Code
+    // https://code.visualstudio.com/docs/java/java-tutorial
+
+// All Java code must be inside a class
+// The name of your class must match your file name
+public class HelloWorld {
+
+    // The main() function is the entry point to your program
+    public static void main(String[] args) {
+        System.out.println("Hello Worlds!");
+        
+    }
+}
