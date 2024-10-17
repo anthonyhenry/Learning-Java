c98e741a3938c90495ff9e2651493b5ba934c499
anthonyhenry <anthonyh1497@gmail.com>
2024-10-16 22:28:19 -0700

    Create DogYears.java

---
+++ 00_IntroductionToJava/src/DogYears.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class DogYears {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int dogYears;
+        int humanYears;
+
+        System.out.print("Enter dog years: ");
+        dogYears = scnr.nextInt();
+        humanYears = 7 * dogYears;
+
+        System.out.print(dogYears);
+        System.out.print(" dog years is about ");
+        System.out.print(humanYears);
+        System.out.println(" human years.");
+    }
+}
