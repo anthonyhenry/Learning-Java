ca08332bc10a16e5218ffc98c96bb58fe646c5c1
anthonyhenry <anthonyh1497@gmail.com>
2024-10-16 20:26:49 -0700

    IntelliJ Hello World

---
+++ IntelliJ/00_HelloWorld/src/Main.java
@@ -0,0 +1,17 @@
+// Setting up a Java development environment
+// To start developing with Java you must first install a Java Development Kit (JDK). You can find the latest by searching JDK online
+// With a JDK you can run .java programs in terminal by navigating to their directory then running javac HelloWorld.java to compile the program, then java HelloWorld.java to execute the program
+
+// IntelliJ is the IDE of choice for programming in Java
+// You can install a free community version of IntelliJ at: https://www.jetbrains.com/idea/download/other.html
+
+
+// All Java code must be inside a class
+// The name of your class must match your file name
+public class Main {
+    // The main() function is the entry point to your program
+    // Start typing "main" and IntelliJ will autocomplete the main function
+    public static void main(String[] args) {
+        System.out.println("Hello from the IntelliJ IDE");
+    }
+}
