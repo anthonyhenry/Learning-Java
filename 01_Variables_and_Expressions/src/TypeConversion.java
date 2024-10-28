d3925cb726466fee632ac39e8e70c50497b3b390
anthonyhenry <anthonyh1497@gmail.com>
2024-10-27 22:14:53 -0700

    Create TypeConversion.java

---
+++ 01_Variables_and_Expressions/src/TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        // Type casting explicitly converts a value of one type to another type.
+        // To type cast precede an expression with (type) to convert the expression's value to the indicated type
+
+        // When type casting make sure to type cast the correct part of the expression
+        System.out.println( (double)(15 / 4) );
+        System.out.println( 15 / (double)4 );
+    }
+}
